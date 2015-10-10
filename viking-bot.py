ca27e59e61fdf8e5a0da086ff30e13b2ee4022d8
rtdmnk <rtdmnk@elknet.info>
2015-10-10 17:03:39 +0200

    sowing potatoes

--- viking-bot.py
+++ viking-bot.py
@@ -1,70 +1,133 @@
 #!/usr/bin/env python3
-import sys
-import socket
-import string
-import re
-import urllib.parse
-import urllib.request
-import json
-import os.path
-import time
-import ssl
+import sys, getopt, re, time, os.path, string, json
+import socket, ssl
+import urllib.parse, urllib.request
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+# globals
+config_file = ""
+commands = []
 
 class bot:
 
     def __init__(self):
         self.config = {}
+        global config_file
+
+        # Dummy values
+        self.host = "chat.freenode.com"
+        self.port = 6667
+        self.ssl = False
+
+        self.nick = "VikingIRCBot"
+        self.ident = "info"
+        self.realname = "Amanita Muscaria"
+
+        self.owner = "Oden"
+        self.log = ""
+
+        self.nickserv = False
+
 
         # if config file exists, parse it
-        if(os.path.isfile("./../viking.conf")):
-            lines = [line.rstrip('\n') for line in open('./../viking.conf')]
+        if(config_file):
+            pass
+        else:
+            config_file = "./vikingBot.conf"
+        if(os.path.isfile(config_file)):
+            lines = [line.rstrip('\n') for line in open(config_file)]
 
             for line in lines:
                 # if comment or empty: pass
                 if(line.startswith("#") or line == ""):
                     pass
                 else:
-                    # remove uneeded things
+                    # parse, get values and add it to the config object
                     line = line.replace(" ", "").replace("\t", "").replace("'", "").replace('"', "")
-                    # get the values
                     line = re.match('(\w+)=(.+)', line)
-                    # add to the config object
                     self.config[line.group(1)] = line.group(2)
+
+            # Put config to corresponding variables
+            self.host = self.config['host']
+
+
+            self.port = int(self.config['port'])
+
+            self.nick = self.config['nick']
+            self.ident = self.config['ident']
+            self.realname = self.config['realname']
+
+            self.owner = self.config['owner']
+
+            self.log = self.config['log_file']
+
+            self.nickserv = self.config['nickserv']
+
+            if("+" in self.config['port']):
+                self.ssl = True
+
+            if(self.nickserv):
+                self.password = self.config['password']
         # if no config found then exit
         else:
-            print("Config file wasn't found (./../viking.conf)")
-            sys.exit()
+            print("Config file wasn't found (" + config_file + ")")
+            string = input("Use dummy settings? [y/n]")
+            if(string.lower() == "y"):
+                pass
+            else:
+                print("quitting")
+                sys.exit()
 
-        self.host = self.config['host']
-        self.port = int(self.config['port'])
 
-        self.nick = self.config['nick']
-        self.ident = self.config['ident']
-        self.realname = self.config['realname']
 
-        self.owner = self.config['owner']
+        self.commands()
 
-        self.log = self.config['log_file']
+    def commands(self):
+        # commands
+        # search engines
+        #
+        google      = command("-g",
+                "-g         <string>                    - Search google")
+        google_i    = command("-gi",
+                "-gi        <string>                    - Search google images")
+        imdb        = command("-imdb",
+                "-imdb      <name +year(optional)>      - Search imdb")
+        wp          = command("-wp",
+                "-wp        <string>                    - Search wikipedia")
 
-        if(self.config['nickserv']):
-            self.password = self.config['password']
+        #
+        # misc
+        #
+        poem        = command("-poem",
+                "-poem                                  - Links the poem of the day")
+        quote       = command("-quote",
+                "-quote                                 - Fetch random quote from forismatic.com")
+
+        #
+        # bot related
+        #
+        bhelp       = command("-help",
+                "-help      <command(optional)>         - Display commands or help for a command")
 
     # Connect and set user information
     def connect(self):
-        simply_s = socket.socket( )
-        self.s = ssl.wrap_socket(simply_s)
+        if(self.ssl):
+            simply_s = socket.socket()
+            self.s = ssl.wrap_socket(simply_s)
+        else:
+            self.s = socket.socket()
+
         self.s.connect((self.host, self.port))
 
         self.s.send(bytes("NICK %s\r\n" % self.nick, "UTF-8"))
         self.s.send(bytes("USER %s %s elknet :%s\r\n" % (self.ident, self.host, self.realname), "UTF-8"))
 
+    # main loop
     def loop(self):
-        # read shit
         readbuffer = ""
-
         while 1:
+            # fetch message, parse it
             readbuffer = readbuffer+self.s.recv(1024).decode("UTF-8")
             temp = str.split(readbuffer, "\n")
             readbuffer=temp.pop( )
@@ -74,36 +137,60 @@ class bot:
 
             # if message recieved
             if(temp[0].find("PRIVMSG") > 0 and not temp[0].startswith(":" + self.host)):
-                # get useful data from it and pass it on
+                # parse it
                 # 1:nick, 2:ident, 3:type, 4:?, 5:message
-                message = re.search(':(.+)!.+@(.+) (\w+) (.+) :(.+)', temp[0])
-                parse_msg(message)
+                msgbuffer = re.search(':(.+)!.+@(.+) (\w+) (.+) :(.+)', temp[0])
+                sender  =   msgbuffer.group(1) # sender of message 
+                ident   =   msgbuffer.group(2) # used to verify nickname if registered to nickserv
+                type    =   msgbuffer.group(3) # we already know this is PRIVMSG
+                channel =   msgbuffer.group(4) # where message was written, can also be a nickname
+                message =   msgbuffer.group(5).replace("\r\n", "").replace("\r", "").replace("\n", "") # message string
+
+                # if owner sends commands through private message
+                if(ident == self.owner and channel == self.nick):
+                    if("join" in message):
+                        chan = message.split("#")
+                        bot_do("join", chan[1])
+                    elif("part" in message):
+                        chan = message.split("#")
+                        bot_do("part", chan[1])
+                    elif("quit" in message):
+                        bot_do("quit", "")
+
+                # if message seems to be a command
+                if(message.startswith("-")):
+                    cmd = re.search("(-\w+)", message)
+                    cmd = cmd.group(1)
+                    try:
+                        string = re.search("-\w+\s+(.+)", message)
+                        string = string.group(1)
+                    except Exception as e:
+                        string = ""
+                    # if message corresponds to command then execute
+                    for command in commands:
+                        if(command.name == cmd):
+                            command.execute(string, channel, sender)
             # if nickserv complains and bot is a registered user
-            elif("NickServ" in temp[0] and "registered" in temp[0] and self.password):
-                self.auth()
+            elif("NickServ" in temp[0] and "registered" in temp[0] and self.nickserv):
+                self.send("", "NickServ", "identify " + self.password)
             # if ping
             elif("PING" in temp[0]):
                 self.send("PONG", "", temp[0])
 
     # make sending messages easier
     def send(self, type, channel, message):
-        if(type == ""):
-            self.s.send(bytes("PRIVMSG %s :%s\r\n" % (channel, message), "UTF-8"))
-            print("PRIVMSG " + channel + ": " + message)
-        else:
+        if(type):
             self.s.send(bytes("%s %s :%s\r\n" % (type, channel, message), "UTF-8"))
             print(type + " " + channel + ": " + message)
+        else:
+            self.s.send(bytes("PRIVMSG %s :%s\r\n" % (channel, message), "UTF-8"))
+            print("PRIVMSG " + channel + ": " + message)
 
-    def auth(self):
-        self.send("", "NickServ", "identify " + self.password)
-
+    # graceful quit
     def quit(self):
         self.s.close()
         sys.exit()
 
-
-# commands
-commands = []
 
 # command class
 class command:
@@ -114,7 +201,7 @@ class command:
 
         commands.append(self)
 
-    def execute(self, string, channel):
+    def execute(self, string, channel, sender):
         cmd = self.name
 
         if(cmd == "-g"):
@@ -128,56 +215,14 @@ class command:
         elif(cmd == "-poem"):
             get_poem(channel)
         elif(cmd == "-quote"):
-            get_quote(channel)
+            get_quote(channel, string)
+        elif(cmd == "-help"):
+            bot_help(sender)
 
-# was command written?
-def parse_msg(unparsed_msg):
 
-    # make stuff more readable
-    sender  =   unparsed_msg.group(1) # sender of message 
-    ident   =   unparsed_msg.group(2) # used to verify nickname if registered to nickserv
-    type    =   unparsed_msg.group(3) # we already know this is PRIVMSG
-    channel =   unparsed_msg.group(4) # where message was written, can also be a nickname
-    message =   unparsed_msg.group(5).replace("\r\n", "").replace("\r", "").replace("\n", "") # message string
-
-    # if owner sends commands through private message
-    if(ident == vbot.owner and channel == vbot.nick):
-        if("join" in message):
-            chan = message.split("#")
-            bot_do("join", chan[1])
-        elif("part" in message):
-            chan = message.split("#")
-            bot_do("part", chan[1])
-        elif("quit" in message):
-            bot_do("quit", "")
-
-    # if message seems to be a command
-    if(message.startswith("-") and not message.startswith("-help")):
-        try:
-            cmz = re.search("(-\w+)", message)
-            cmd = cmz.group(1)
-
-            # if command doesnt need input
-            if(cmd == "-poem" or cmd == "-quote"):
-                string = ""
-            else:
-                cmz = re.search("(-\w+)\s+(.+)", message)
-                string = cmz.group(2)
-
-        except Exception as e:
-            vbot.send("", channel, "Bad input, see -help")
-            error("parse_msg", e)
-            return
-
-        for command in commands:
-            if(command.name == cmd):
-                command.execute(string, channel)
-
-    # if mentioned or -help
-    elif(message.startswith("-help")):
-        bot_help(sender)
-
-# bot functions
+#----------------------------
+# Command functions
+#----------------------------
 def bot_help(sender):
     vbot.send("", sender, "In case you didn't know, I'm a (beta) bot, shocker. I can do the following:")
 
@@ -244,35 +289,48 @@ def search_wp(search_string, chan):
         results = json.loads(search_results)
 
         title = results['query']['pages'][0]['title']
-        summary = results['query']['pages'][0]['extract'][:100] + "..."
-        data = title + " [https://en.wikipedia.org/wiki/" + title +"]"
+        summary = results['query']['pages'][0]['extract']
+        summary = re.match("^(.\.[A-z]\..+?)\.\s", summary).group(1) + "."
+
+        # if summary is over 160 chars, shorten
+        if(len(summary) > 160):
+            summary = summary[:157] + "..."
+        data = summary + " [https://en.wikipedia.org/wiki/" + urllib.parse.quote(title) +"]"
         vbot.send("", chan, data)
-        vbot.send("", chan, summary)
     except Exception as e:
         vbot.send("", chan, "Jag fann inge")
         error("search_wp", e)
         return
 
 def get_poem(chan):
-    url = 'http://poems.com/today.php'
-    html = urlopen(url)
-    soup = BeautifulSoup(html, 'html.parser')
-
-    title = soup.find(id="page_title").text
-    author = soup.find(id="byline").a.text
-
-    data = "Todays poem: '" + title + "' by " + author + " [" + url + "]"
-    vbot.send("", chan, data)
-
-def get_quote(chan):
-    url = 'http://api.theysaidso.com/qod.json'
-    response = urllib.request.urlopen(url)
-    results = response.read().decode("utf8")
-    results = json.loads(results)
     try:
-        quote = results['contents']['quotes'][0]['quote']
-        author = results['contents']['quotes'][0]['author']
-        data = "'" + quote + "' - " + author + ' [https://theysaidso.com/]'
+        url = 'http://poems.com/today.php'
+        html = urlopen(url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        title = soup.find(id="page_title").text
+        author = soup.find(id="byline").a.text
+
+        data = "Todays poem: '" + title + "' by " + author + " [" + url + "]"
+        vbot.send("", chan, data)
+    except Exception as e:
+        vbot.send("", chan, "Nåt gick fel")
+        error("get_poem", e)
+        return
+
+def get_quote(chan, string):
+    url = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en'
+    try:
+        # get data
+        response = urllib.request.urlopen(url)
+        results = response.read().decode("utf8")
+        results = json.loads(results)
+
+        # handle data
+        quote = results['quoteText']
+        quote = re.sub(" $", "", quote)
+        author = results['quoteAuthor']
+        data = "'" + quote + "' - " + author
         vbot.send("", chan, data)
     except Exception as e:
         vbot.send("", chan, "Nåt gick fel")
@@ -284,37 +342,38 @@ def error(place, e):
     # print error to term
     print('\033[1;41m[%s|%s] Caught exception: %s\033[1;m' % (date,place,e))
     # if log file specified
-    if(vbot.log):
-        with open(vbot.log, 'a') as log:
-            log.write('[%s|%s] Caught exception: %s\n' % (date,place,e))
+    try:
+        if(vbot.log):
+            with open(vbot.log, 'a') as log:
+                log.write('[%s|%s] Caught exception: %s\n' % (date,place,e))
+    except Exception as e:
+        print(e)
 
-# commands
-# search engines
-#
-google      =   command("-g",
-                    "-g         <string>                    - Search google")
-google_i    =   command("-gi",
-                    "-gi        <string>                    - Search google images")
-imdb        =   command("-imdb",
-                    "-imdb      <name +year(optional)>      - Search imdb")
-wp          =   command("-wp",
-                    "-wp        <string>                    - Search wikipedia")
+def check_args():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:v", ["config="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        #if o == "-v":
+        #    verbose = True
+        if o in ("-c", "--config"):
+            global config_file
+            config_file = a
+        else:
+            assert False, "unhandled option"
 
-#
-# misc
-#
-poem        =   command("-poem",
-                    "-poem                                  - Links the poem of the day")
-quote       =   command("-quote",
-                    "-quote                                 - Get quote of the day")
 
-#
-# bot related
-#
-bhelp       =   command("-help",
-                    "-help      <command(optional)>         - Display commands or help for a command")
+if __name__ == '__main__':
+    # check for command line arguments
+    check_args()
 
-# start it all
-vbot = bot()
-vbot.connect()
-vbot.loop()
+    # give life to bot
+    vbot = bot()
+    vbot.connect()
+    vbot.loop()
